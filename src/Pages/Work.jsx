1ad59c8218c72a7ba4aa055ff3c54ecf9329c9aa
AgilPrayoga <ariagilprayoga@gmail.com>
2026-04-20 05:43:44 +0800

    perbaikan error end

--- src/Pages/Work.jsx
+++ src/Pages/Work.jsx
@@ -3,7 +3,6 @@ import Footer from "../Components/Footer"
 import Project1 from"../assets/project1.png"
 import Project2 from"../assets/project2.png"
 import Project3 from"../assets/project3.png"
-import { NavLink } from "react-router-dom"
 import { motion as m } from "framer-motion"
 import 'flowbite'
 
@@ -17,16 +16,16 @@ const Work= () =>{
         <div className=" overflow-auto bg-[#2C3333] flex justify-center items-center flex-col pt-28 sm:pt-0 ">
             <m.h1 className="font-extrabold font-montserrat text-[40px] mt-28 my-3 text-[#d5ceb6] sm:mb-6 "
             
-            initial={{ opacity: 0, y:500  }}
-            animate={{  opacity: [0, 1, 0, 1],x:[0],y:[500,0] }}
-            transition={{ease:'anticipate',duration:2}}
-            exit={{ opacity: [0, 1, 0, 1, 0] ,x:[500,0] }}
+            initial={{ opacity: 0, y: 100 }}
+animate={{ opacity: 1, y: 0 }}
+transition={{ ease: "anticipate", duration: 0.8 }}
+exit={{ opacity: 0, y: 50 }}
             
             >PROFESSIONAL <span  className="text-[#2C3333] bg-[#d5ceb6] ">PROJECTS</span></m.h1>
             <div className="flex flex-col sm:flex-row justify-center items-center">
                 <div className="sm:mr-28  flex justify-center items-center mb-2 flex-col">
                     <div >
-                            <a href={Project1} target="_blank" rel="noopener noreferrer"><img className="max-w-[400px]" src={Project1} alt=""/></a>
+                            <a href={Project1} target="_blank" rel="noopener noreferrer"><img className="max-w-[400px]" src={Project1} alt="Preview Project DBON Kaltim"/></a>
                             <div className="mt-2 flex justify-center items-center  border border-[#F0E9D2] p-2">
                 <h1 className='font-extrabold text-[20px] font-montserrat text-[#2C3333] bg-[#d5ceb6] hover:text-[#d5ceb6] hover:bg-[#2C3333] ' >Profile DBON Kaltim </h1>
                 
@@ -36,7 +35,7 @@ const Work= () =>{
                 </div>
                 <div className="sm:mr-28 mb-2">
                     <div >
-                           <a href={Project2} target="_blank" rel="noopener noreferrer"> <img className="max-w-[300px]" src={Project2} alt=""/></a>
+                           <a href={Project2} target="_blank" rel="noopener noreferrer"> <img className="max-w-[300px]" src={Project2} alt="Preview Project DBON Kaltim"/></a>
                             <div className="mt-2 flex justify-center items-center  border border-[#F0E9D2] p-2">
                 <h1 className='font-extrabold text-[20px] font-montserrat text-[#2C3333] bg-[#d5ceb6] hover:text-[#d5ceb6] hover:bg-[#2C3333] ' >Aplikasi Pembayaran </h1>
                 
@@ -46,7 +45,7 @@ const Work= () =>{
                 </div>
                 <div>
                     <div>
-                            <a href={Project3} target="_blank" rel="noopener noreferrer"><img className="max-w-[300px]" src={Project3} alt=""/></a>
+                            <a href={Project3} target="_blank" rel="noopener noreferrer"><img className="max-w-[300px]" src={Project3} alt="Preview Project DBON Kaltim"/></a>
  <div className="mt-2 flex justify-center items-center  border border-[#F0E9D2] p-2">
                 <h1 className='font-extrabold text-[20px] font-montserrat text-[#2C3333] bg-[#d5ceb6] hover:text-[#d5ceb6] hover:bg-[#2C3333] ' >Aplikasi IT Helpdesk  </h1>
                 
@@ -57,46 +56,45 @@ const Work= () =>{
            
             <m.h1 className="font-extrabold font-montserrat text-[40px] my-3 text-[#d5ceb6] sm:mb-6 "
             
-            initial={{ opacity: 0, y:500  }}
-            animate={{  opacity: [0, 1, 0, 1,],x:[0],y:[500,0] }}
-            transition={{ease:'anticipate',duration:2}}
-            exit={{ opacity: [0, 1, 0, 1, 0] ,x:[500,0] }}
+            initial={{ opacity: 0, y: 100 }}
+animate={{ opacity: 1, y: 0 }}
+transition={{ ease: "anticipate", duration: 0.8 }}
+exit={{ opacity: 0, y: 50 }}
             
             >ACADEMIC <span  className="text-[#2C3333] bg-[#d5ceb6] ">PROJECTS</span></m.h1>
             <div className="flex flex-col sm:flex-row justify-center items-center">
                 <div className="sm:mr-28  flex justify-center items-center mb-2 flex-col">
-                    <div class="embed-responsive embed-responsive-21by9">
+                    <div className="embed-responsive embed-responsive-21by9">
                         <iframe  src="https://www.youtube.com/embed/rgpFN3p9YgI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
                     </div>
                     
                 </div>
                 <div className="sm:mr-28 mb-2">
-                    <div class="embed-responsive embed-responsive-21by9">
+                    <div className="embed-responsive embed-responsive-21by9">
                         <iframe  src="https://www.youtube.com/embed/-1DbcLiqOKw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
                     </div>
                 </div>
                 <div>
-                    <div class="embed-responsive embed-responsive-21by9">
+                    <div className="embed-responsive embed-responsive-21by9">
                         <iframe  src="https://www.youtube.com/embed/VLBQDGy6IyY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
                     </div>
                 </div>
             </div>
             <m.div
-            initial={{ opacity: 0, y:500  }}
-            animate={{  opacity: [0, 1, 0, 1,],x:[0],y:[500,0] }}
-            transition={{ease:'anticipate',duration:2}}
-            exit={{ opacity: [0, 1, 0, 1, 0] ,x:[500,0] }}
-            
+          initial={{ opacity: 0, y: 100 }}
+animate={{ opacity: 1, y: 0 }}
+transition={{ ease: "anticipate", duration: 0.8 }}
+exit={{ opacity: 0, y: 50 }}
             >
                 <div className="mt-6 flex justify-center items-center border border-[#F0E9D2] p-2">
                 <span to={"/work/project1"} className='font-extrabold text-[20px] font-montserrat text-[#2C3333] bg-[#d5ceb6] hover:text-[#d5ceb6] hover:bg-[#2C3333] ' >PROJECT 1 </span>
-                <a href={'https://github.com/AgilPrayoga/SobatLoker'} target="blank" className=''> 
+                <a href={'https://github.com/AgilPrayoga/SobatLoker'} target="_blank" className=''> 
                     <svg xmlns="http://www.w3.org/2000/svg" className="fill-[#F0E9D2] ml-2 hover:fill-white"  viewBox="0 0 50 50" width="30px" height="30px">    <path d="M17.791,46.836C18.502,46.53,19,45.823,19,45v-5.4c0-0.197,0.016-0.402,0.041-0.61C19.027,38.994,19.014,38.997,19,39 c0,0-3,0-3.6,0c-1.5,0-2.8-0.6-3.4-1.8c-0.7-1.3-1-3.5-2.8-4.7C8.9,32.3,9.1,32,9.7,32c0.6,0.1,1.9,0.9,2.7,2c0.9,1.1,1.8,2,3.4,2 c2.487,0,3.82-0.125,4.622-0.555C21.356,34.056,22.649,33,24,33v-0.025c-5.668-0.182-9.289-2.066-10.975-4.975 c-3.665,0.042-6.856,0.405-8.677,0.707c-0.058-0.327-0.108-0.656-0.151-0.987c1.797-0.296,4.843-0.647,8.345-0.714 c-0.112-0.276-0.209-0.559-0.291-0.849c-3.511-0.178-6.541-0.039-8.187,0.097c-0.02-0.332-0.047-0.663-0.051-0.999 c1.649-0.135,4.597-0.27,8.018-0.111c-0.079-0.5-0.13-1.011-0.13-1.543c0-1.7,0.6-3.5,1.7-5c-0.5-1.7-1.2-5.3,0.2-6.6 c2.7,0,4.6,1.3,5.5,2.1C21,13.4,22.9,13,25,13s4,0.4,5.6,1.1c0.9-0.8,2.8-2.1,5.5-2.1c1.5,1.4,0.7,5,0.2,6.6c1.1,1.5,1.7,3.2,1.6,5 c0,0.484-0.045,0.951-0.11,1.409c3.499-0.172,6.527-0.034,8.204,0.102c-0.002,0.337-0.033,0.666-0.051,0.999 c-1.671-0.138-4.775-0.28-8.359-0.089c-0.089,0.336-0.197,0.663-0.325,0.98c3.546,0.046,6.665,0.389,8.548,0.689 c-0.043,0.332-0.093,0.661-0.151,0.987c-1.912-0.306-5.171-0.664-8.879-0.682C35.112,30.873,31.557,32.75,26,32.969V33 c2.6,0,5,3.9,5,6.6V45c0,0.823,0.498,1.53,1.209,1.836C41.37,43.804,48,35.164,48,25C48,12.318,37.683,2,25,2S2,12.318,2,25 C2,35.164,8.63,43.804,17.791,46.836z"/></svg>
                 </a>
              </div>
             <div className="mt-2 flex justify-center items-center  border border-[#F0E9D2] p-2">
                 <span to={"/work/project2"} className='font-extrabold text-[20px] font-montserrat text-[#2C3333] bg-[#d5ceb6] hover:text-[#d5ceb6] hover:bg-[#2C3333] ' >PROJECT 2 </span>
-                <a href={'https://github.com/AgilPrayoga/AntiqueGallery'} target="blank" className=''> 
+                <a href={'https://github.com/AgilPrayoga/AntiqueGallery'} target="_blank" className=''> 
                     <svg xmlns="http://www.w3.org/2000/svg" className="fill-[#F0E9D2] ml-2 hover:fill-white"  viewBox="0 0 50 50" width="30px" height="30px">    <path d="M17.791,46.836C18.502,46.53,19,45.823,19,45v-5.4c0-0.197,0.016-0.402,0.041-0.61C19.027,38.994,19.014,38.997,19,39 c0,0-3,0-3.6,0c-1.5,0-2.8-0.6-3.4-1.8c-0.7-1.3-1-3.5-2.8-4.7C8.9,32.3,9.1,32,9.7,32c0.6,0.1,1.9,0.9,2.7,2c0.9,1.1,1.8,2,3.4,2 c2.487,0,3.82-0.125,4.622-0.555C21.356,34.056,22.649,33,24,33v-0.025c-5.668-0.182-9.289-2.066-10.975-4.975 c-3.665,0.042-6.856,0.405-8.677,0.707c-0.058-0.327-0.108-0.656-0.151-0.987c1.797-0.296,4.843-0.647,8.345-0.714 c-0.112-0.276-0.209-0.559-0.291-0.849c-3.511-0.178-6.541-0.039-8.187,0.097c-0.02-0.332-0.047-0.663-0.051-0.999 c1.649-0.135,4.597-0.27,8.018-0.111c-0.079-0.5-0.13-1.011-0.13-1.543c0-1.7,0.6-3.5,1.7-5c-0.5-1.7-1.2-5.3,0.2-6.6 c2.7,0,4.6,1.3,5.5,2.1C21,13.4,22.9,13,25,13s4,0.4,5.6,1.1c0.9-0.8,2.8-2.1,5.5-2.1c1.5,1.4,0.7,5,0.2,6.6c1.1,1.5,1.7,3.2,1.6,5 c0,0.484-0.045,0.951-0.11,1.409c3.499-0.172,6.527-0.034,8.204,0.102c-0.002,0.337-0.033,0.666-0.051,0.999 c-1.671-0.138-4.775-0.28-8.359-0.089c-0.089,0.336-0.197,0.663-0.325,0.98c3.546,0.046,6.665,0.389,8.548,0.689 c-0.043,0.332-0.093,0.661-0.151,0.987c-1.912-0.306-5.171-0.664-8.879-0.682C35.112,30.873,31.557,32.75,26,32.969V33 c2.6,0,5,3.9,5,6.6V45c0,0.823,0.498,1.53,1.209,1.836C41.37,43.804,48,35.164,48,25C48,12.318,37.683,2,25,2S2,12.318,2,25 C2,35.164,8.63,43.804,17.791,46.836z"/></svg>
                 </a>
              </div>
